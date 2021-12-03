710ff9308333e95b3df4a3840c6da065bd67b15e
teslah22 <jonasz.jalocha@gmail.com>
2021-12-03 01:21:22 +0100

    Add all missing files.

---
+++ main/static/assets/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1.5,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 2000,
+    center: true
+
+    });
+  });
+
+
+// $('.container .owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     nav: false,
+//     items: 2
+
+// })
+
+// $('.testimonials .owl-carousel').owlCarousel({
+//   loop: true,
+//   margin: 20,
+//   nav: false,
+//   responsive: {
+//       0: {
+//           items: 1
+//       },
+//       600: {
+//           items: 2
+//       },
+//       1000: {
+//           items: 3
+//       }
+//   }
+
+// })
