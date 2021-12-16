accfead27f461e7723a6e7ba578bf12bdfbfc95b
teslah22 <jonasz.jalocha@gmail.com>
2021-12-16 03:20:10 +0100

    Before deploy #6.

---
+++ staticfiles/assets/js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $("#resume .container .owl-carousel").owlCarousel({
+
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    autoplay: SVGComponentTransferFunctionElement,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 2000,
+    center: true
+    });
+
+    $("#projects .container .owl-carousel").owlCarousel({
+
+      loop: true,
+      margin: 0,
+      nav: true,
+      items: 1,
+      autoplay: SVGComponentTransferFunctionElement,
+      autoplayTimeout: 6000,
+      autoplayHoverPause: true,
+      autoplaySpeed: 2000,
+      center: true
+      });
+
+  });
+
+
+// $('.container .owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     nav: false,
+//     items: 2
+
+// })
+
+// $('.testimonials .owl-carousel').owlCarousel({
+//   loop: true,
+//   margin: 20,
+//   nav: false,
+//   responsive: {
+//       0: {
+//           items: 1
+//       },
+//       600: {
+//           items: 2
+//       },
+//       1000: {
+//           items: 3
+//       }
+//   }
+
+// })
